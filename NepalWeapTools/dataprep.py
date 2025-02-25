b6855932d48078e28c528abad830f4fb48589bca
Richard Farr <richard.farr@alluvium.com.au>
2025-02-26 10:44:49 +1100

    Add shell for dataprep module with basic metadata and header

---
+++ NepalWeapTools/dataprep.py
@@ -0,0 +1,23 @@
+"""
+------------------------------------------------------------------------
+Module Name: dataprep
+Parent Package: NepalWeapTools
+
+******* Metadata *******
+__author__ = ['Kristen Joyce', 'Richard Farr']
+__copyright__ = 'Alluvium Consulting (TBC)'
+__credits__ = ['Petter Nyman']
+__version__ = 0.01
+__maintainer__ = 'Richard Farr'
+__email__ = 'richard.farr@alluvium.com.au'
+__status__ = 'In development'
+
+Last update: 26/02/2025
+
+******* Description *******
+Part of the NepalWeapTools package, this module takes inputs of various 
+catchment variable as datsets and converts them into output formats
+that are readable by the Stockholm Environment Institute's Water
+Evaluation And Planning (WEAP) software.
+------------------------------------------------------------------------
+"""
