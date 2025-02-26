5bf1dacde4753171d9b35431d23936b303ee4a64
Richard Farr <richard.farr@alluvium.com.au>
2025-02-26 11:12:06 +1100

    Add imports and placeholder main() function

--- NepalWeapTools/dataprep.py
+++ NepalWeapTools/dataprep.py
@@ -21,3 +21,21 @@ that are readable by the Stockholm Environment Institute's Water
 Evaluation And Planning (WEAP) software.
 ------------------------------------------------------------------------
 """
+####### Module Imports: #######
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+####### For when module is run directly: #######
+def main():
+    """
+    Placeholder function for if this package is run directly.
+    Running directly is not currently in scope
+    """
+    print('dataprep module has been run directly. \nPlease use within the\
+    context of a python script and the NepalWeapTools package.')
+    pass
+    
+if __name__ == '__main__':
+    main()
