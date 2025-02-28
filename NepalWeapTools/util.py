cbf7c1e79e37d3abcf1553f18ab13f4721e3eb78
Richard Farr <richard.farr@alluvium.com.au>
2025-02-28 11:14:59 +1100

    Create shell for util module

---
+++ NepalWeapTools/util.py
@@ -0,0 +1,21 @@
+"""
+------------------------------------------------------------------------
+Module Name: util
+Parent Package: NepalWeapTools
+
+******* Metadata *******
+__author__ = ['Richard Farr']
+__copyright__ = 'Alluvium Consulting (TBC)'
+__credits__ = ['Kristen Joyce', 'Petter Nyman']
+__version__ = 0.01
+__maintainer__ = 'Richard Farr'
+__email__ = 'richard.farr@alluvium.com.au'
+__status__ = 'In development'
+
+Last update: 28/02/2025
+
+******* Description *******
+Part of the NepalWeapTools package, this module contains functions
+commonly required by the classes in the other package modules
+------------------------------------------------------------------------
+"""
