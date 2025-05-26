026dba5e8051effeaab6c0ef0cd23a27fa7bdbf9
Richard Farr <richard.farr@alluvium.com.au>
2025-05-26 14:16:46 +1000

    plot_water_balance() working as Kristen wrote

--- NepalWeapTools/outputvis.py
+++ NepalWeapTools/outputvis.py
@@ -70,8 +70,6 @@ def plot_weap_outputs(
     - When selecting data for streamflow_exceedance, check the 'Percent
     of Time Exceeded' and 'Log' buttons in the 'Streamflot Gauge
     Comparison' section.
-    - Date column values must be in a valid ISO8601 format as per
-    datetime.datetime.fromisoformat()
     
     --------------------------------------------------------------------
     """
@@ -145,14 +143,13 @@ def plot_weap_outputs(
     #Save to OutputData if save is True
     if save:
         plt.savefig(
-            'Summary outputs.png',
+            os.path.join(output_loc, 'Summary outputs.png'),
             dpi=300,
             bbox_inches='tight'
             )
     
     #-------------------------------------------------------------------
     # STREAMFLOW COMPARISON FIGURE
-    
     # load streamflow comparison data
     sfc_data = pd.read_excel(comparison_path)
 
@@ -198,13 +195,13 @@ def plot_weap_outputs(
     #Save to output data if save is True:
     if save:
         plt.savefig(
-            'Streamflow comparison.png',
+            os.path.join(output_loc, 'Streamflow comparison.png'),
             dpi=300,
             bbox_inches='tight'
             )
+            
     #-------------------------------------------------------------------
     # STREAMFLOW EXCEEDANCE FIGURE
-
     # load data of streamflow exceedances
     sf_exc = pd.read_excel(exceedance_path)
 
@@ -239,8 +236,180 @@ def plot_weap_outputs(
 
     if save:
         plt.savefig(
-            'Streamflow exceedance.png',
+            os.path.join(output_loc, 'Streamflow exceedance.png'),
             dpi=300,
             bbox_inches='tight'
             )
 
+####### Water balance: ########################################################
+#-----------------------------------------------------------------------
+def plot_water_balance(
+    catchment_data:str,
+    supply_demand_data:str,
+    save:bool=False
+    ):
+    """
+    Create plot of catchment water availability compared to demands and
+    supply capacities
+    
+    Parameters:
+    - catchment_data (str): filename.ext for Excel file with daily
+    outputs from the 'Land Class Inflows and Outflows' section of the
+    WEAP results tab
+    - supply_demand_data (str): filename.ext for for Excel file with
+    daily values of demands and supply capacities for urban and
+    irrigation. Demand values taken from the WEAP input files for urban
+    and irrigation demands. Supply capacities taken from utility tables
+    - save (bool): whether to save the figures in the OutputData folder,
+    defaults to False.
+    
+    --------------------------------------------------------------------
+    Notes:
+    - WEAP output files must be cleaned PRIOR to running this function.
+    The steps required are as follows:
+        1. Delete sum columns and rows
+        2. Delete the first three header rows
+        3. Ensure the date column is labelled 'Date'
+        4. Change streamflow volums to megalitres
+    - Excel files must be stored in the InputData\WeapOutputs package
+    folder
+    - When selecting data for catchment_data, only delineated
+    sub-catchments (not demand nodes) were selected. Values were also
+    updated from litres to megalitres prior to importing.
+    
+    --------------------------------------------------------------------
+    """
+    #Get the directory relative to the current script (outputvis.py)
+    current_dir = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+        )
+    #Construct the path to the InputData folder
+    input_data_path = os.path.join(current_dir, r'InputData\WeapOutputs')
+    #Add the file names to the path:
+    catchment_path = os.path.join(input_data_path, catchment_data)
+    supdem_path = os.path.join(input_data_path, supply_demand_data)
+    #Same for output location:
+    output_loc = os.path.join(current_dir, r'OutputData')
+    
+    #-------------------------------------------------------------------
+    # CALCULATE MONTHLY AVGS - CATCHMENT RAINFALL RUNOFF WATER BALANCE 
+    # read data
+    catch_data = pd.read_excel(catchment_path)
+
+    #set date column to datetime format
+    catch_data['Date'] = pd.to_datetime(catch_data['Date'])
+    catch_data['month'] = catch_data['Date'].dt.month
+    
+    #Convert to monthly averages:
+    catch_monthly_avgs = catch_data.groupby('month').mean(numeric_only=True)
+
+    #Save monthly averages to Excel:
+    catch_monthly_avgs.to_excel(
+        os.path.join(output_loc, 'catchment_monthly_avgs.xlsx'),
+        index=True
+        )
+    
+    #-------------------------------------------------------------------
+    # CALCULATE MONTHLY AVGS - SUPPLY AND DEMAND WATER BALANCE
+    # read input data
+    data = pd.read_excel(supdem_path)
+
+    # set date column in datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    data['month'] = data['Date'].dt.month
+
+    # get monthly averages of the summary data
+    supdem_monthly_avgs = data.groupby('month').mean(numeric_only=True)
+
+    # calculate catchment water availability as sum on surface runoff
+    #and base flow
+    supdem_monthly_avgs['Catchment water availability'] = (
+        catch_monthly_avgs['Base Flow'] + catch_monthly_avgs['Surface Runoff']
+        )
+    #Make values positive:
+    supdem_monthly_avgs['Catchment water availability'] = (
+        supdem_monthly_avgs['Catchment water availability'].abs()
+        )
+    
+    #Save to Excel:
+    supdem_monthly_avgs.to_excel(
+        os.path.join(output_loc, 'supply_demand_monthly_avgs.xlsx'),
+        index=True
+        )
+
+    #-------------------------------------------------------------------
+    # MERGE CATCHMENT AND SUPPLY/DEMAND DATA TO GET SINGLE SUMMARY 
+    #DATASET
+    merged_data = pd.merge(
+        catch_monthly_avgs,
+        supdem_monthly_avgs,
+        on='month',
+        how='inner'
+        )
+    
+    #Save combined file to excel:
+    merged_data.to_excel(
+        os.path.join(output_loc, 'water_balance_assessment.xlsx'),
+        index=True
+        )
+    
+    #-------------------------------------------------------------------
+    # CREATE THE BAR CHART
+    plt.figure(figsize = (10, 5))
+    columns = supdem_monthly_avgs.columns
+    # num_colors = len(color_list)
+    bar_width = 0.15
+    
+    plt.bar(
+        supdem_monthly_avgs.index - (2 * bar_width),
+        supdem_monthly_avgs['Catchment water availability'],
+        bar_width,
+        label='Catchment water availability',
+        color='royalblue'
+        )
+    plt.bar(
+        supdem_monthly_avgs.index - (1 * bar_width),
+        supdem_monthly_avgs['Urban demand'],
+        bar_width,
+        label='Urban demand',
+        color='maroon'
+        )
+    plt.bar(
+        supdem_monthly_avgs.index,
+        supdem_monthly_avgs['Urban surface water supply yield'],
+        bar_width,
+        label='Urban surface water supply yield',
+        color='indianred'
+        )
+    plt.bar(
+        supdem_monthly_avgs.index + (1 * bar_width),
+        supdem_monthly_avgs['Urban groundwater supply yield'],
+        bar_width,
+        label='Urban groundwater supply yield',
+        color='tomato'
+        )
+    plt.bar(
+        supdem_monthly_avgs.index + (2 * bar_width),
+        supdem_monthly_avgs['Irrigation demand'],
+        bar_width,
+        label='Irrigation demand',
+        color='gold'
+        )
+    
+    #Format the chart:
+    plt.xlabel('Month', fontsize = 12)
+    plt.ylabel('Water volumes (ML)', fontsize = 12)
+    plt.xticks(ticks=range(1, 13), labels=[
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ])
+    plt.yscale('log')
+    plt.legend()
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(
+            os.path.join(output_loc, 'Water balance by month.png'),
+            dpi=300,
+            bbox_inches='tight'
+            )
