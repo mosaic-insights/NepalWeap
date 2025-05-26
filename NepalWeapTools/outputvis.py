2253fa1d28ef5e6d66f4a47c465563aa4661f5bb
Richard Farr <richard.farr@alluvium.com.au>
2025-05-26 12:22:43 +1000

    Create shell for outputvis module

---
+++ NepalWeapTools/outputvis.py
@@ -0,0 +1,28 @@
+"""
+------------------------------------------------------------------------
+Module Name: outputvis
+Parent Package: NepalWeapTools
+
+******* Metadata *******
+__author__ = ['Kristen Joyce', 'Richard Farr']
+__copyright__ = 'Alluvium Consulting (TBC)'
+__credits__ = ['Petter Nyman']
+__version__ = 0.01
+__maintainer__ = 'Richard Farr'
+__email__ = 'richard.farr@alluvium.com.au'
+__status__ = 'In development'
+
+Last update: 26/05/2025
+
+******* Description *******
+Part of the NepalWeapTools package, this module takes the ouput files
+produced by WEAP and plots them for visual interpretation.
+------------------------------------------------------------------------
+"""
+####### Package imports: ######################################################
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
